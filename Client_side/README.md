3a1d052def06d25d251912c615bf2725bf548722
= <=>
2023-07-05 00:27:46 +0545

    SQL not working

---
+++ Client_side/README.md
@@ -0,0 +1,14 @@
+# Chat-Application
+
+This application is still under development
+
+To use this application, you need to run 2 scripts, one for the client side and one for the server side
+
+Using ngrok to host server.py
+1. Install ngrok
+2. Run ngrok tcp <port> (In server.py, port is 8000. If you use different port, you need to edit it in server.py as well) 
+3. You will get a forwarding link such as tcp://0.tcp.in.ngrok.io:17416. Ignore tcp://
+4. Your SERVER_HOST will be 0.tcp.in.ngrok.io
+5. Your SERVER_PORT will be 17416
+6. update client.py, change SERVER_HOST and SERVER_PORT. Then, start server.py
+7. Distribute client.py, share it with friends. Your friends will be able to connect to your server
---
+++ Client_side/app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request,redirect
+import sqlite3
+import socket
+import random
+from threading import Thread
+from datetime import datetime
+from colorama import Fore, init, Back
+
+# initialize colors
+init()
+
+colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX,
+          Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX,
+          Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTWHITE_EX,
+          Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.RED, Fore.WHITE, Fore.YELLOW
+          ]
+
+# choose a random color for the connected client
+client_color = random.choice(colors)
+
+# # server's IP address
+# SERVER_HOST = "0.0.0.0"  # tcp server hosted at ngrok or other app
+# SERVER_PORT = 8000 # port at tcp server
+# separator_token = "<SEP>"
+
+# # initialize TCP socket
+# s = socket.socket()
+# print(f"[*] Connecting to {SERVER_HOST}:{SERVER_PORT}...")
+
+# # connect to the server
+# s.connect((SERVER_HOST, SERVER_PORT))
+# print("[+] Connected.")
+
+# set up sqlite database
+app = Flask(__name__)
+con = sqlite3.connect('chats.db', check_same_thread=False)
+db = con.cursor()
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/send", methods=['GET', 'POST'])
+def send():
+    if request.method == "GET":
+        return
+    else:
+        # get the message sent by user
+        global msg
+        msg = request.form.get('msg')
+        print("Sent a message")
+        print(msg)
+        
+        usn = "prason"
+        time_now = datetime.now().strftime('%H:%M:%S')
+        # to_send = f"{client_color}[{time_now}] {usn}: {msg}{Fore.RESET}"
+        # s.send(to_send.encode())
+        
+        data = [usn, msg, time_now]
+        db.execute("INSERT INTO chat (usn, msg, timestamp) VALUES (?, ?, ?);", data)
+
+        return redirect("/")
